fd68fb78291566fbf7af8f5369add43a42f0bb74
Rudianto Sihombing <45420193+rudi97277@users.noreply.github.com>
2023-09-07 05:03:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const anova = require('ml-anova')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    const data= req.body.data
+    const classes = req.body.classes
+    const result = anova.oneWay(data,classes)
+    res.json(result)
 })
 app.listen(process.env.PORT || 3000)
