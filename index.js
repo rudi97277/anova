4c4286a60455f0f57bbd86c8e583fc7ec801e74b
Rudianto Sihombing <rudi97277@gmail.com>
2023-09-07 08:21:58 +0700

    feat: fix edited

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const anova = require('ml-anova')
-const app = express()
-app.all('/', (req, res) => {
-    //const data= req.body.data
-    //const classes = req.body.classes
-    //const result = anova.oneWay(data,classes)
-    res.json(req.body)
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const anova = require("ml-anova");
+const app = express();
+const { urlencoded, json } = require("body-parser");
+
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+app.all("/", (req, res) => {
+  const data = req.body.data;
+  const classes = req.body.classes;
+  const result = anova.oneWay(data, classes, { alpha: 0.05 });
+  res.json(result);
+});
+app.listen(process.env.PORT || 3000);
